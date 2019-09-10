8f9bef4a2c18727e5d9b517777bef58a8cd34c90
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-09-09 21:27:56 -0300

    because I have many curiosity

--- server.py
+++ server.py
@@ -3,14 +3,22 @@ import Pyro4
 
 @Pyro4.expose
 class TicketFactory(object):
+    def __init__(self):
+        global cont_server
+        if cont_server > 0:
+            self.cont_instance = cont_server
+        else:
+            self.cont_instance = 0
 
     def tickets(self):
-        global cont
-        cont += 1
-        return "Your ticket: ", cont
+        global cont_server
+        self.cont_instance = cont_server
+        self.cont_instance += 1
+        cont_server = self.cont_instance
+        return "Your ticket: ", cont_server
 
 
-cont = 0
+cont_server = 0
 daemon = Pyro4.Daemon()
 uri = daemon.register(TicketFactory)
 
